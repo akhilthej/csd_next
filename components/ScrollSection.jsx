2ac13ef7dfc372d282722744842d59728a69356b
akhilthej <67720804+akhilthej@users.noreply.github.com>
2024-01-23 02:03:28 +0530

    1

--- components/ScrollSection.jsx
+++ components/ScrollSection.jsx
@@ -213,10 +213,10 @@ function ScrollSection() {
                   <h2 className="cursor-default mt-8 upanddown font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600 leading-[3.5rem] tracking-tight">
                     Web Development
                   </h2>
-                  <p className=" text-sm opacity-50">
+                  <p className="text-sm opacity-50">
                     Every business requires a unique website to effectively
                     reach its customers or clients. At Cyber Space Digital,
-                    we're here to help you discover the best choices tailored to
+                    were here to help you discover the best choices tailored to
                     your needs.
                   </p>
                   Build your business global today
@@ -288,7 +288,7 @@ function ScrollSection() {
                   <p className=" text-sm opacity-50">
                     Every business requires a unique website to effectively
                     reach its customers or clients. At Cyber Space Digital,
-                    we're here to help you discover the best choices tailored to
+                    were here to help you discover the best choices tailored to
                     your needs.
                   </p>
                   Build your business global today
@@ -360,7 +360,7 @@ function ScrollSection() {
                   <p className=" text-sm opacity-50">
                     Every business requires a unique website to effectively
                     reach its customers or clients. At Cyber Space Digital,
-                    we're here to help you discover the best choices tailored to
+                    were here to help you discover the best choices tailored to
                     your needs.
                   </p>
                   Build your business global today
@@ -432,7 +432,7 @@ function ScrollSection() {
                   <p className=" text-sm opacity-50">
                     Every business requires a unique website to effectively
                     reach its customers or clients. At Cyber Space Digital,
-                    we're here to help you discover the best choices tailored to
+                    were here to help you discover the best choices tailored to
                     your needs.
                   </p>
                   Build your business global today
@@ -504,7 +504,7 @@ function ScrollSection() {
                   <p className=" text-sm opacity-50">
                     Every business requires a unique website to effectively
                     reach its customers or clients. At Cyber Space Digital,
-                    we're here to help you discover the best choices tailored to
+                    were here to help you discover the best choices tailored to
                     your needs.
                   </p>
                   Build your business global today
@@ -571,12 +571,12 @@ function ScrollSection() {
                 {/* First Row */}
                 <div className="text-center ">
                   <h2 className="cursor-default mt-8 upanddown font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600 leading-[3.5rem] tracking-tight">
-                    Mobile App's
+                    Mobile Apps
                   </h2>
                   <p className=" text-sm opacity-50">
                     Every business requires a unique website to effectively
                     reach its customers or clients. At Cyber Space Digital,
-                    we're here to help you discover the best choices tailored to
+                    were here to help you discover the best choices tailored to
                     your needs.
                   </p>
                   Build your business global today
